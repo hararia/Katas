2a705874a3a32e771d68a3b429bce3230acde9bd
hararia <thedrew88@gmail.com>
2020-07-26 14:22:40 -0400

    divisibleInts 6kyu kata

---
+++ 6kyu/divisibleInts.js
@@ -0,0 +1,33 @@
+//solution 2
+
+function getCount(n){
+    let nStr = n.toString()
+    let counter = 0
+    
+    for (let j = 0; j<nStr.length; j++)
+      for (let i = j; i<=nStr.length; i++)
+        if(n % nStr.slice(j, i) == 0 && nStr.slice(j, i) != nStr) counter++
+  
+    return counter
+  }
+
+  //solution 1
+
+  function getCount(n){
+    let nStr = n.toString()
+    let counter = 0
+    
+    for (let j = 0; j<nStr.length; j++){
+      for (let i = j; i<=nStr.length; i++){
+        let subStr = nStr.slice(j, i)
+        
+        if (subStr == nStr){
+          continue
+        }
+        if(n % subStr == 0){
+          counter++
+        }
+      }
+    }
+    return counter
+  }
--- 6kyu/equalSidesOfAnArray.js
+++ 6kyu/equalSidesOfAnArray.js
@@ -1,4 +1,4 @@
-//solution 1
+//solution refacotred
 
 function findEvenIndex(arr){
   
@@ -10,4 +10,26 @@ function findEvenIndex(arr){
     return -1
   }
 
+  // solution 1
+
+  function findEvenIndex(arr){
+    let leftSideSum = 0
+    let rightSideSum = 0
   
+  for(let i = 0; i<arr.length; i++){
+    
+    leftSideSum = arr.slice(0,i).reduce((acc,value)=>{
+      return acc+value
+    },0)
+    
+    rightSideSum = arr.slice(i+1).reduce((acc,value)=>{
+      return acc+value
+    },0)
+    
+    if(leftSideSum===rightSideSum)
+      return i
+    
+  }
+  return -1
+}
+
