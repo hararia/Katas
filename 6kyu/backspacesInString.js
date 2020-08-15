ca45758c460b34276c1225676ffcc28145ca9909
hararia <thedrew88@gmail.com>
2020-08-15 19:19:34 -0400

    new 6kyu kata

---
+++ 6kyu/backspacesInString.js
@@ -0,0 +1,30 @@
+// https://www.codewars.com/kata/5727bb0fe81185ae62000ae3
+
+//solution 1
+function cleanString(s) {
+    let result = []
+    for (const char of s){
+      if (char == '#'){
+        result.pop()
+      }
+      else{
+        result.push(char)
+      }
+    }
+    return result.join('')
+  };
+
+//solution 2
+function cleanString(s) {
+    s=s.split('')
+    for (let i=0;i<s.length;i++){
+      if (s[i]=='#'){
+        s[i]=''
+        s[i-1]=''
+        s=s.join('').split('')
+        i=i-2
+      }
+  }
+  return s.join('')
+};
+
